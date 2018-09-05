a9c0101f21dd724237b0bc7b1ad34348b808df2f
steven@forban.io <steven@MacBook-Air-de-viardot.local>
2018-09-05 20:11:21 +0200

    page offres

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,10 @@ class PagesController < ApplicationController
     @posts = Post.all.order("created_at DESC")
   end
 
+  def offers
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 5)
+  end
+
   def contact
   end
 end
